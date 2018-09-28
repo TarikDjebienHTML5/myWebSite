4e7b44e8054503609bb064e4489331feb0bbabbc
DJEBIEN Tarik <tarik.djebien@gmail.com>
2018-09-29 00:39:25 +0200

    Externalize Qr code JS

---
+++ resources/js/contact.js
@@ -0,0 +1,17 @@
+/*
+ * My Web Site Javascript function
+ */
+$(document).ready(function() {
+	$("#sendEmail").click(function(){
+	    if($("#qrcodeForm #subject").val() && $("#qrcodeForm #body").val()){
+	        $("#qrcodeImg img").remove();
+            		var subject = $("#qrcodeForm #subject").val();
+            		var body = $("#qrcodeForm #body").val();
+            		var message = "mailto:tarik.djebien@gmail.com?subject="+subject+"!&body="+body;
+            		$("#qrcodeImg").qrcode({
+            			render: 'image',
+            			text: message
+            		});
+	    }
+	});
+});
--- resources/js/mywebsite.js
+++ resources/js/mywebsite.js
@@ -4,14 +4,4 @@
 $(document).ready(function() {
 	clock.init();
 	clock.start();
-	$("#sendEmail").click(function(){
-		$("#qrcodeImg img").remove();
-		var subject = $("#qrcodeForm #subject").val();
-		var body = $("#qrcodeForm #body").val();
-		var message = "mailto:tarik.djebien@gmail.com?subject="+subject+"!&body="+body;
-		$("#qrcodeImg").qrcode({
-			render: 'image',
-			text: message
-		});
-	});
 });
